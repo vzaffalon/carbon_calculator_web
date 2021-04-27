38c0a46373afac4f1d43eb0517a4a8906ac1e987
Victor Marra <zaffalonvictor@gmail.com>
2021-04-27 15:52:24 -0300

    Finished css

--- src/screens/categories/CategoriesScreen.tsx
+++ src/screens/categories/CategoriesScreen.tsx
@@ -3,9 +3,9 @@ const { Title, Text } = Typography;
 const { TabPane } = Tabs;
 
 function CategoriesScreen(){
-    return <div style={{width: "100%", justifyContent: "center"}}>
-        <Title level={3} style={{textAlign: "center", marginTop: 20}}>Carbon Footprint Calculator</Title>
-        <Tabs defaultActiveKey="1" style={{width: "100%"}} centered>
+    return <div style={{width: "100%", justifyContent: "center", alignItems: "center"}}>
+        <Title level={2} style={{textAlign: "center", marginTop: 30}}>Carbon Footprint Calculator</Title>
+        <Tabs type="line" tabBarStyle={{color: "#40a9ff", width: 1200, marginLeft: 120}} defaultActiveKey="1" style={{width: "100%"}} centered>
             <TabPane tab="Housing" key="1">
                 <Row style={{justifyContent: "center" }}>
                     <div>
@@ -18,9 +18,9 @@ function CategoriesScreen(){
                                     <Card type="inner" title={"Natural gas"}>
                                         <div>
                                             <Input style={{marginBottom: 5}} placeholder="10" />
-                                            <Text style={{marginLeft: "105px"}}>kWh/yr</Text>
+                                            <Text style={{marginLeft: "105px", color: "#8C8C8C"}}>kWh/yr</Text>
                                         </div>
-                                        <div style={{height: 50, background: "#75b66c", alignItems: "center", justifyContent: "center", display: "flex", margin: "20px -24px -16px -24px"}}>
+                                        <div style={{height: 50, background: "#73d13d", alignItems: "center", justifyContent: "center", display: "flex", margin: "20px -24px -16px -24px"}}>
                                             <Text strong style={{color: "#ffffff", fontSize: "15px"}}>18 kg CO2e/yr</Text>
                                         </div>
                                     </Card>
@@ -32,9 +32,9 @@ function CategoriesScreen(){
                         <Card title="Total Category Emissions" style={{ width: 300, height: "100%", marginLeft: 10 }}>
                                 <div style={{alignItems: "center", justifyContent: "center", display: "flex", height: "350px"}}>
                                     <Col style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                                        <Text style={{fontSize: 42, fontWeight: 400}}>Housing</Text>
+                                        <Text style={{fontSize: 22, fontWeight: 400, color: "#8C8C8C"}}>Housing</Text>
                                         <Text style={{fontSize: 30}}>10,500</Text>
-                                        <Text style={{fontSize: 20}}>Annual CO2 emissions (Kg.)</Text>
+                                        <Text style={{fontSize: 14, color: "#8C8C8C"}}>Annual CO2 emissions (Kg.)</Text>
                                     </Col>
                                 </div>
                         </Card>
@@ -49,32 +49,33 @@ function CategoriesScreen(){
             </TabPane>
         </Tabs>
         <div style={{alignItems: "center", justifyContent: "center", width: "100%", display: "flex", marginTop: 10, marginBottom: 60}}>
-            <Card type="inner" title="Your Carbon Footprint Annual CO2 emissions (Kg.)" style={{ width: 1210, height: 230 }}>
-                <Row style={{alignItems: "center", justifyContent: "space-between", height: 130}}>
-                    <Col span={4} style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        <Text style={{fontSize: 28}}>Housing</Text>
+            <Card type="inner" title="Categories Carbon Footprint Annual CO2 emissions (Kg.)" style={{ width: 800, height: 230 }}>
+                <Row style={{alignItems: "center", justifyContent: "space-between", height: 140, marginRight: 70, marginLeft: 50}}>
+                    <Col style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <Text style={{fontSize: 22, color: "#8C8C8C"}}>Housing</Text>
                         <Text style={{fontSize: 28}}>10,500</Text>
-                        <Text style={{fontSize: 16}}>U.S. Average*: 10,500</Text>
+                        <Text style={{fontSize: 12, color: "#8C8C8C"}}>U.S. Average*: 10,500</Text>
                     </Col>
 
-                    <Col span={4} style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        <Text style={{fontSize: 28}}>Travel</Text>
+                    <Col style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <Text style={{fontSize: 22, color: "#8C8C8C"}}>Travel</Text>
                         <Text style={{fontSize: 28}}>10,590</Text>
-                        <Text style={{fontSize: 16}}>U.S. Average*: 10,500</Text>
+                        <Text style={{fontSize: 12, color: "#8C8C8C"}}>U.S. Average*: 10,500</Text>
                     </Col>
 
-                    <Col span={4} style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        <Text style={{fontSize: 28}}>Waste</Text>
+                    <Col style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <Text style={{fontSize: 22, color: "#8C8C8C"}}>Waste</Text>
                         <Text style={{fontSize: 28}}>10,500</Text>
-                        <Text style={{fontSize: 16}}>U.S. Average*: 10,500</Text>
-                    </Col>
-
-                    <Col span={12} style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        <Text style={{fontSize: 34}}>Total</Text>
-                        <Text style={{fontSize: 34}}>10,500</Text>
-                        <Text style={{fontSize: 24}}>U.S. Average*: 10,500</Text>
+                        <Text style={{fontSize: 12, color: "#8C8C8C"}}>U.S. Average*: 10,500</Text>
                     </Col>
                 </Row>
+            </Card>
+            <Card type="inner" title="Total Carbon Footprint Annual CO2 emissions (Kg.)" style={{ width: 400, height: 230, marginLeft: 10 }}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: 20}}>
+                        <Text style={{fontSize: 20, color: "#8C8C8C"}}>Total</Text>
+                        <Text style={{fontSize: 34}}>10,500</Text>
+                        <Text style={{fontSize: 14, color: "#8C8C8C"}}>U.S. Average*: 10,500</Text>
+                </div>
             </Card>
         </div>
   </div>
